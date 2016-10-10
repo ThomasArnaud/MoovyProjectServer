221df45f1c1c56112ca29a1473bc62228c261d6b
Bruno Buiret <bruno.b@apps-panel.com>
2016-10-10 10:17:16 +0200

    Hibernate

--- src/main/java/com/moovy/server/utils/HibernateUtil.java
+++ src/main/java/com/moovy/server/utils/HibernateUtil.java
@@ -1,5 +1,11 @@
 package com.moovy.server.utils;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 /**
  * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
  * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
@@ -7,4 +13,36 @@ package com.moovy.server.utils;
  */
 public class HibernateUtil
 {
+    /**
+     * The Hibernate session factory.
+     */
+    protected static SessionFactory factory = null;
+
+    /**
+     * Opens a new Hibernate session.
+     *
+     * @return A Hibernate session.
+     */
+    public static Session openSession()
+    {
+        if(HibernateUtil.factory == null)
+        {
+            StandardServiceRegistry registry =
+                new StandardServiceRegistryBuilder()
+                .configure()
+                .build()
+            ;
+
+            try
+            {
+                HibernateUtil.factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            }
+            catch(Exception e)
+            {
+                StandardServiceRegistryBuilder.destroy(registry);
+            }
+        }
+
+        return HibernateUtil.factory.openSession();
+    }
 }
