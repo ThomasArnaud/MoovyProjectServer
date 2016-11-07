758dcaa7f845f1fc0c245a7dbaa2ffc892d32adb
Alexdef74307 <arabilloud@hotmail.fr>
2016-11-07 11:00:14 +0100

    Model Category/Film

--- src/main/java/com/moovy/server/model/Category.java
+++ src/main/java/com/moovy/server/model/Category.java
@@ -4,4 +4,25 @@ package com.moovy.server.model;
  * Created by thomas on 07/11/2016.
  */
 public class Category {
+
+    protected String code;
+
+    protected String name;
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/main/java/com/moovy/server/model/Film.java
+++ src/main/java/com/moovy/server/model/Film.java
@@ -1,7 +1,118 @@
 package com.moovy.server.model;
 
+import java.util.Date;
+import java.util.List;
+
 /**
- * Created by thomas on 07/11/2016.
+ * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
+ * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
+ * @author Alexis Rabilloud (alexis.rabilloud@etu.univ-lyon1.fr)
  */
-public class Film {
+public class Film
+{
+    protected int id;
+
+    protected String title;
+
+    protected int duration;
+
+    protected Date releaseDate;
+
+    protected int budget;
+
+    protected int profit;
+
+    protected Director director;
+
+    protected List<Character> characters;
+
+    protected List<Category> categories;
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public int getDuration()
+    {
+        return this.duration;
+    }
+
+    public void setDuration(int duration)
+    {
+        this.duration = duration;
+    }
+
+    public Date getReleaseDate()
+    {
+        return this.releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate)
+    {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getBudget()
+    {
+        return this.budget;
+    }
+
+    public void setBudget(int budget)
+    {
+        this.budget = budget;
+    }
+
+    public int getProfit()
+    {
+        return this.profit;
+    }
+
+    public void setProfit(int profit)
+    {
+        this.profit = profit;
+    }
+
+    public Director getDirector()
+    {
+        return this.director;
+    }
+
+    public void setDirector(Director director)
+    {
+        this.director = director;
+    }
+
+    public List<Character> getCharacters()
+    {
+        return this.characters;
+    }
+
+    public void setCharacters(List<Character> characters)
+    {
+        this.characters = characters;
+    }
+
+    public List<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
 }
